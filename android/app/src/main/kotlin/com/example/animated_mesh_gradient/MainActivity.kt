5b0b446c7f365c4a0c5ed6ec8d1eb894724a93ff
Souvik Biswas <sbis1999@gmail.com>
2024-07-07 09:08:10 +0530

    Initialize

---
+++ android/app/src/main/kotlin/com/example/animated_mesh_gradient/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animated_mesh_gradient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'animated_mesh_gradient_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AnimatedMeshGradientPage();
+  }
+}
